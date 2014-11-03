d5c092486999eaa9058b6849fa0a344a38dfe3d8
oskar.asbrink@gmail.com <oskar.asbrink@gmail.com>
2014-11-03 16:31:04 +0100

    tweak sim configs

--- src/defaults/DefaultSimulators.js
+++ src/defaults/DefaultSimulators.js
@@ -42,6 +42,27 @@ define({
 				"max": 1.0
 
 			}
+		},
+		{
+			"id": "AdditiveTrail",
+			"atlas":"defaultSpriteAtlas",
+			"renderers": [
+				"TrailRenderer"
+			],
+			"poolCount": 2000,
+			"blending": {
+				"value": "AdditiveBlending",
+				"type": "option",
+				"values": ["AdditiveBlending", "SubtractiveBlending", "MultiplyBlending", "NoBlending", "CustomBlending"],
+				"texts": ["AdditiveBlending", "SubtractiveBlending", "MultiplyBlending", "NoBlending", "CustomBlending"]
+			},
+			"alphakill": {
+				"value": 0,
+				"type": "number",
+				"min": 0.0,
+				"max": 1.0
+
+			}
 		}
 	]
 });
